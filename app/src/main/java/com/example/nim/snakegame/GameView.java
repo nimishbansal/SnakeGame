8ee04c53e3158c6d694432d275a46b2860d3fd6d
nimish <nimish4july1998@gmail.com>
2019-05-26 23:28:22 +0530

    initial commit surface view

---
+++ app/src/main/java/com/example/nim/snakegame/GameView.java
@@ -0,0 +1,72 @@
+package com.example.nim.snakegame;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
+    private CharacterSprite characterSprite;
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        characterSprite = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.avdgreen));
+
+
+        thread.setRunning(true);
+        thread.start();
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    public void update() {
+        characterSprite.update();
+
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+
+        super.draw(canvas);
+        if(canvas!=null) {
+            characterSprite.draw(canvas);
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nim/snakegame/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.nim.snakegame
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : Activity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(GameView(this))
+    }
+}
